e04c681aa59253c6eaf284c34cf8cf957cce1356
Arno Joosen <arno.joosen@outlook.com>
2025-03-26 22:23:35 +0100

    fix camera

--- launch/spawn_robot.launch.py
+++ launch/spawn_robot.launch.py
@@ -325,7 +325,7 @@ def generate_launch_description():
         package='ros_gz_image',
         executable='image_bridge',
         namespace=robot_namespace,
-        arguments=['/camera/image_raw'],
+        arguments=['camera/image_raw'],
         output='screen'
     )
 
